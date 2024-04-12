e0db326ed46b2999b7122649dbe54bfba2d8060f
SushilMallRC <sushil.mall@ringcentral.com>
2024-04-12 11:24:52 +0530

    added a class for test

---
+++ src/main/java/com/ringcentral/NewJavaDOcTestClass.java
@@ -0,0 +1,16 @@
+/**
+ * Package name is com.rigcentral
+ */
+package com.ringcentral;
+
+/**
+ * Calss Name is NewJavaDOcTestClass
+ */
+public class NewJavaDOcTestClass {
+    /**
+     * Testing JavaDoc
+     */
+    public void testJavaDoc(){
+        System.out.println("Hello Java Doc");
+    }
+}
